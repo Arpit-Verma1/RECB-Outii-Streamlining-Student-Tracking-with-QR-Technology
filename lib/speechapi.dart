56a3870a7a63ee6cac62bb7cfe3c5d34144ae128
Arpit Verma <99536186+arpitvermaofficial@users.noreply.github.com>
2023-02-10 18:48:25 +0530

    Update speechapi.dart

--- lib/speechapi.dart
+++ lib/speechapi.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 
-class Speechapi{
-static final _speech=SpeechToText();
+class Speechapi {
+  static final _speech = SpeechToText();
 
-static Future<bool>togglerecording({required Function(String text) onresult,
-required ValueChanged<bool>onlistening})async{
-  if(_speech.isListening){
-    _speech.stop();return true;
+  static Future<bool> togglerecording(
+      {required Function(String text) onresult,
+      required ValueChanged<bool> onlistening}) async {
+    if (_speech.isListening) {
+      _speech.stop();
+      return true;
+    }
+    final isAvailable = await _speech.initialize(
+        onStatus: (status) => onlistening(_speech.isListening),
+        onError: (e) => print('Error: $e'));
+    if (isAvailable) {
+      _speech.listen(onResult: (value) => onresult(value.recognizedWords));
+    }
+    return isAvailable;
   }
-  final isAvailable=await _speech.initialize(onStatus: (status)=>onlistening(_speech.isListening),
-  onError: (e)=>print('Error: $e'));
-  if(isAvailable){
-    _speech.listen(onResult: (value)=>onresult(value.recognizedWords));
-  }
-  return isAvailable;
-}
 }
