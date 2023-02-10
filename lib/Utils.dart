719d18b053ea03bc033a85fd011c4a1bff49bfa9
Arpit Verma <99536186+arpitvermaofficial@users.noreply.github.com>
2023-02-10 18:54:23 +0530

    Update Utils.dart

--- lib/Utils.dart
+++ lib/Utils.dart
@@ -4,27 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+class Utils {
 
-class Utils{
-  static Future<String>downloadfile(String url,String filename)async{
-    final directory=await getApplicationDocumentsDirectory();
-    final filepath='${directory.path}/${filename}';
-    final response=await http.get(Uri.parse(url));
-    final file=File(filepath);
-    await file.writeAsBytes(response.bodyBytes);
-    return filepath;
-  }
-  static GlobalKey<ScaffoldMessengerState> messengerKey=GlobalKey<ScaffoldMessengerState>();
-  static showSnackBar(String?text){
-
-    if(text==null)return;
-    final snackbar=SnackBar(content:Text(text),backgroundColor: Colors.red);
+  static GlobalKey<ScaffoldMessengerState> messengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+  static showSnackBar(String? text) {
+    if (text == null) return;
+    final snackbar = SnackBar(content: Text(text), backgroundColor: Colors.red);
     messengerKey.currentState!
       ..removeCurrentSnackBar()
       ..showSnackBar(snackbar);
   }
-
 }
-
-
-
