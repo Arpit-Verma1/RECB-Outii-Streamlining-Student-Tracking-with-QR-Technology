1ae1cd7463d0ce1cb4655a88d9c17ec53ef0f8aa
Arpit Verma <99536186+arpitvermaofficial@users.noreply.github.com>
2023-02-10 18:36:50 +0530

    Update add user.dart

--- lib/add user.dart
+++ lib/add user.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'qrpage.dart';
+import 'Student_Qr_Scan.dart';
+
 class add extends StatefulWidget {
   const add({Key? key}) : super(key: key);
 
@@ -9,70 +10,71 @@ class add extends StatefulWidget {
 }
 
 class _addState extends State<add> {
- TextEditingController controller=TextEditingController();
- TextEditingController controller1=TextEditingController();
- late DatabaseReference _dbref;
- String text="";
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller1 = TextEditingController();
+  late DatabaseReference _dbref;
+  String text = "";
 
- void initState(){
-   super.initState();
-   _dbref=FirebaseDatabase.instance.ref();
-
- }
+  void initState() {
+    super.initState();
+    _dbref = FirebaseDatabase.instance.ref();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255,245,189,159),
-      appBar: AppBar(
-
-      ),
-      body:     Container(width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.only(left: 30,right: 30,top: 20),child:
-      Column(crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-       TextField(
-            controller: controller,
-            style: TextStyle(color: Colors.black),
-            decoration: new InputDecoration(
-              enabledBorder:  OutlineInputBorder(borderRadius: BorderRadius.circular(15),borderSide: BorderSide(color:Colors.black,width: 2)),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15.0),
-                borderSide: BorderSide(
-                    color: Colors.black
-                    ,width: 2
+        backgroundColor: Color.fromARGB(255, 245, 189, 159),
+        appBar: AppBar(),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.only(left: 30, right: 30, top: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                controller: controller,
+                style: TextStyle(color: Colors.black),
+                decoration: new InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.black, width: 2)),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: BorderSide(color: Colors.black, width: 2),
+                  ),
+                  prefix: Text("    "),
+                  suffixIcon: IconButton(
+                      onPressed: () => _createdb(), icon: Icon(Icons.add)),
+                  contentPadding: EdgeInsets.only(top: 10, bottom: 10),
+                  filled: false,
+                  hintText: 'Add User Email Id',
+                  hintStyle: TextStyle(color: Colors.black),
                 ),
               ),
-              prefix: Text("    "),suffixIcon: IconButton(onPressed:()=> _createdb() , icon:Icon(Icons.add)),
-              contentPadding:
-              EdgeInsets.only(top: 10,bottom: 10),
-              filled: false,
-              hintText: 'Add User Email Id',
-              hintStyle: TextStyle(color: Colors.black),
-            ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.2,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    remove();
+                  },
+                  child: Text('remove')),
+            ],
           ),
-              SizedBox(width: MediaQuery.of(context).size.width*0.2,)
-              ,ElevatedButton(onPressed: (){
-                remove();
-              }, child: Text('remove')),
-
-
-
-
-
-        ],
-      ),)
-    );
+        ));
   }
- _createdb(){
-   _dbref.child("User").update({controller.text.substring(
-       0, controller.text.indexOf('.')):controller.text});
- }
- remove(){
-   _dbref.child("User").child(controller.text.substring(
-       0, controller.text.indexOf('.'))).remove();
- }
 
+  _createdb() {
+    _dbref.child("User").update({
+      controller.text.substring(0, controller.text.indexOf('.')):
+          controller.text
+    });
+  }
 
-
+  remove() {
+    _dbref
+        .child("User")
+        .child(controller.text.substring(0, controller.text.indexOf('.')))
+        .remove();
+  }
 }
