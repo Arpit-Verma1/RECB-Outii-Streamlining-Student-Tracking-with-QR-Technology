1e1058749d1e1f1b6010c3ea1e8b342a07f0eb59
Arpit Verma <99536186+arpitvermaofficial@users.noreply.github.com>
2023-02-10 18:37:20 +0530

    Update auth.dart

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'Utils.dart';
-import 'login.dart';
-import 'Signup.dart';
+import 'Login_Page.dart';
+import 'SignUp_page.dart';
 import 'main.dart';
+
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key, required this.islogin}) : super(key: key);
   final bool islogin;
@@ -11,11 +12,12 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  bool isLogin=true;
+  bool isLogin = true;
   @override
-  Widget build(BuildContext context)=>isLogin?Login(onclickedsignup: toggle):
-  SignupWidget( onclickedsignin: toggle,);
-  void toggle()=>setState(()=>isLogin=!isLogin);
+  Widget build(BuildContext context) => isLogin
+      ? Login(onclickedsignup: toggle)
+      : SignupWidget(
+          onclickedsignin: toggle,
+        );
+  void toggle() => setState(() => isLogin = !isLogin);
 }
-
-
