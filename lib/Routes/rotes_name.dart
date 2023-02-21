e1ab35994f4ec049122351b68c68464cdcc3d77d
Arpit Verma <99536186+arpitvermaofficial@users.noreply.github.com>
2023-02-22 00:59:31 +0530

    Add files via upload

---
+++ lib/Routes/rotes_name.dart
@@ -0,0 +1,8 @@
+class RouteName{
+  static const String My_HomePage="Splashview";
+  static const String sample="login_screen";
+  static const String add_user="home_screen";
+  static const String Qr_generate="signup";
+  static const String forgot_passwd="Splashview";
+
+}
