6e5c1942dbe0bbc101195aa2c27e296fe704f001
Arpit Verma <99536186+arpitvermaofficial@users.noreply.github.com>
2023-05-05 15:02:48 +0530

    Feature:- view all months frequency chart view

--- lib/View/student_previous_data.dart
+++ lib/View/student_previous_data.dart
@@ -5,15 +5,17 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:outii/Component/Widgets/widgets.dart';
 import 'package:outii/Userjson.dart';
 import 'package:fl_chart/fl_chart.dart';
-import '../Bar_Chart_View.dart';
+import '../Bar_Chart.dart';
 import 'package:intl/intl.dart';
 
 TextStyle textStyle = TextStyle(fontSize: 13, fontWeight: FontWeight.bold);
 List<BarChartGroupData> get barGroups => barGroups1;
 List<int> value = List<int>.filled(31, 0, growable: false);
 List<String> dates = [];
+
 class Student_previous_data extends StatefulWidget {
-  const Student_previous_data({Key? key, required this.Collection, required this.Phone_No})
+  const Student_previous_data(
+      {Key? key, required this.Collection, required this.Phone_No})
       : super(key: key);
   final String Collection;
   final String Phone_No;
@@ -24,9 +26,30 @@ class Student_previous_data extends StatefulWidget {
 class _Student_previous_dataState extends State<Student_previous_data>
     with TickerProviderStateMixin {
   @override
+  String dropdownvalue = DateFormat(
+    "MMM",
+  ).format(DateTime.now()).toString();
+  bool recollect_bardata = true;
+  bool change_bardata = true;
+  bool chart_view = true;
+  // List of items in our dropdown menu
+  var items = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
   late final AnimationController _fadeController;
   late final Animation<double> _fadeAnimation;
-  bool chart_view = false;
+
   void initState() {
     super.initState();
     readuser(widget.Collection);
@@ -57,74 +80,61 @@ class _Student_previous_dataState extends State<Student_previous_data>
   }
 
   Widget build(BuildContext context) {
-    String dt = DateFormat(
-      "MMM",
-    ).format(DateTime.now());
     Size size = MediaQuery.of(context).size;
-    Widget buildsuser(User user) {
-      for (int i = 1; i < 32; i++) {
-        String zero="";
-        if(i<10){
-          zero="0";
-        }
-        if (user.Timein!.contains("$zero$i-$dt")) {
-          if (dates.contains(user.Timein) == true) {
-            value[i - 1]--;
-            dates.remove(user.Timein!);
-          } else {
-            value[i - 1]++;
-            dates.add(user.Timein!);
-          }
-        }
-      }
-      return FadeTransition(
-        opacity: _fadeAnimation,
-        child: Container(
-          margin: EdgeInsets.only(top: 10, left: 10, right: 10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
-            border: Border.all(color: Colors.black),
-            color: Color.fromARGB(255, 198, 216, 255),
-          ),
-          child: ListTile(
-            leading: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '${user.Timein!.substring(0, 6)}',
-                  style: textStyle.copyWith(color: Colors.black),
+    Widget build_list(User user) {
+      String dt = dropdownvalue;
+      char_frequency_counter(user, dt);
+      return user.Timein!.contains(dt) == true
+          ? FadeTransition(
+              opacity: _fadeAnimation,
+              child: Container(
+                margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                  border: Border.all(color: Colors.black),
+                  color: Color.fromARGB(255, 198, 216, 255),
                 ),
-                Text(
-                  '${user.Timein!.substring(
-                    6,
-                  )}',
-                  style: textStyle.copyWith(color: Colors.black),
+                child: ListTile(
+                  leading: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${user.Timein!.substring(0, 6)}',
+                        style: textStyle.copyWith(color: Colors.black),
+                      ),
+                      Text(
+                        '${user.Timein!.substring(
+                          6,
+                        )}',
+                        style: textStyle.copyWith(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                  trailing: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${user.Timeout?.substring(0, 6)}',
+                        style: textStyle.copyWith(color: Colors.black),
+                      ),
+                      Text(
+                        '${user.Timeout?.substring(
+                          6,
+                        )}',
+                        style: textStyle.copyWith(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                  title: Text(
+                    '${user.Where}',
+                    style: textStyle.copyWith(color: Colors.black),
+                  ),
                 ),
-              ],
-            ),
-            trailing: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '${user.Timeout!.substring(0, 6)}',
-                  style: textStyle.copyWith(color: Colors.black),
-                ),
-                Text(
-                  '${user.Timeout!.substring(
-                    6,
-                  )}',
-                  style: textStyle.copyWith(color: Colors.black),
-                ),
-              ],
-            ),
-            title: Text(
-              '${user.Where}',
-              style: textStyle.copyWith(color: Colors.black),
-            ),
-          ),
-        ),
-      );
+              ),
+            )
+          : Container();
     }
 
     return Scaffold(
@@ -140,19 +150,97 @@ class _Student_previous_dataState extends State<Student_previous_data>
                 });
                 setState(() {});
               },
-              icon: Icon(chart_view ? Icons.account_circle : Icons.bar_chart))
+              icon: chart_view
+                  ? Icon(Icons.account_circle)
+                  : Row(
+                      children: [
+                        Icon(Icons.bar_chart),
+                      ],
+                    ))
         ],
+        titleSpacing: 0,
         title: Text(
           "Rajkiya Engineering College Bijnor",
           style: GoogleFonts.cookie(
               fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
+      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
         Visibility(
           visible: !chart_view,
           child: Column(children: [
+            Row(
+              children: [
+                Text(
+                  DateFormat(
+                        "yyyy",
+                      ).format(DateTime.now()) +
+                      " " +
+                      dropdownvalue +
+                      " Month Frequency Chart",
+                  style: GoogleFonts.lobster(color: Colors.black, fontSize: 20),
+                ),
+                DropdownButton(
+                  menuMaxHeight: 300,
+                  value: dropdownvalue,
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  items: items.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(
+                        items,
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownvalue = newValue!;
+                      for (int i = 0; i < 31; i++) value[i] = 0;
+                      dates.clear();
+                      recollect_bardata = !recollect_bardata;
+                      change_bardata = !change_bardata;
+                      Future.delayed(Duration(seconds: 1), () {
+                        recollect_bardata = !recollect_bardata;
+                        setState(() {
+                          change_bardata = !change_bardata;
+                        });
+                      });
+                      Future.delayed(Duration(seconds: 2), () {
+                        setState(() {
+                          change_bardata = !change_bardata;
+                          change_bardata = !change_bardata;
+                        });
+                      });
+                    });
+                  },
+                ),
+              ],
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Visibility(
+                visible: change_bardata,
+                replacement: Container(
+                    height: size.height * 0.2,
+                    child: Center(
+                        child: CircularProgressIndicator(
+                      color: Colors.black,
+                    ))),
+                child: Container(
+                    width: size.width * 2,
+                    margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+                    padding: EdgeInsets.only(top: 12, right: 12),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 229, 228, 226),
+                      border: Border.all(),
+                    ),
+                    height: size.height * 0.2,
+                    child: Bar_Chart()),
+              ),
+            ),
+          ]),
+          replacement: Column(children: [
             Text(
               "${widget.Collection.substring(0, widget.Collection.length - 7)} "
                       .toUpperCase() +
@@ -199,41 +287,11 @@ class _Student_previous_dataState extends State<Student_previous_data>
                             ),
                             listtile(
                               "Phone No.",
-                             widget.Phone_No
-                                  .toUpperCase(),
+                              widget.Phone_No.toUpperCase(),
                             )
                           ],
                         )),
                   ]),
-            ),
-          ]),
-        ),
-        Visibility(
-          visible: chart_view,
-          child: Column(children: [
-            Text(
-              DateFormat(
-                    "yyyy",
-                  ).format(DateTime.now()) +
-                  " " +
-                  DateFormat(
-                    "MMMM",
-                  ).format(DateTime.now()) +
-                  " Month Frequency Chart",
-              style: GoogleFonts.lobster(color: Colors.black, fontSize: 20),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Container(
-                  width: size.width * 2,
-                  margin: EdgeInsets.only(top: 10, left: 10, right: 10),
-                  padding: EdgeInsets.only(top: 12, right: 12),
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 229, 228, 226),
-                      border: Border.all(),
-                     ),
-                  height: size.height * 0.2,
-                  child: Bar_Chart()),
             ),
           ]),
         ),
@@ -255,29 +313,54 @@ class _Student_previous_dataState extends State<Student_previous_data>
           ],
         ),
         Container(
-          height: size.height * 0.57,
-          child: StreamBuilder<List<User>>(
-            stream: readuser(widget.Collection),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Text(
-                    "Somrthing  Went Wrong ${snapshot.error.toString()}");
-              } else if (snapshot.hasData) {
-                final users = snapshot.data!;
-                return ListView(
-                  scrollDirection: Axis.vertical,
-                  children: users.map(buildsuser).toList(),
-                );
-              } else {
-                return Center(
-                  child: CircularProgressIndicator(color: Colors.black),
-                );
-              }
-            },
+          height: size.height * 0.56,
+          child: Visibility(
+            visible: recollect_bardata,
+            replacement:
+                Center(child: CircularProgressIndicator(color: Colors.black)),
+            child: StreamBuilder<List<User>>(
+              stream: readuser(widget.Collection),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text(
+                      "Somrthing  Went Wrong ${snapshot.error.toString()}");
+                } else if (snapshot.hasData) {
+                  final users = snapshot.data!;
+                  return ListView(
+                    scrollDirection: Axis.vertical,
+                    children: users.map(build_list).toList(),
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(color: Colors.black),
+                  );
+                }
+              },
+            ),
           ),
-        ),
+        )
       ]),
     );
+  }
+
+  void char_frequency_counter(User user, String dt) {
+    if (recollect_bardata) {
+      for (int i = 1; i < 32; i++) {
+        String zero = "";
+        if (i < 10) {
+          zero = "0";
+        }
+        if (user.Timein!.contains("$zero$i-$dt")) {
+          if (dates.contains(user.Timein) == true) {
+            value[i - 1]--;
+            dates.remove(user.Timein!);
+          } else {
+            value[i - 1]++;
+            dates.add(user.Timein!);
+          }
+        }
+      }
+    }
   }
 
   Stream<List<User>> readuser(String Collection) => FirebaseFirestore.instance
